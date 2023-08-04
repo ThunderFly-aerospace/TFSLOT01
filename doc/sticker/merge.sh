5b7b85d544ca1271d88f93c478bae2c43804180e
Roman Dvorak <romandvorak@mlab.cz>
2023-08-04 13:27:47 +0200

    TFSLOT sticker design

---
+++ doc/sticker/merge.sh
@@ -0,0 +1,2 @@
+pdftk design_a_design.pdf design_a_crop.pdf cat output TFSLOT_sticker_A.pdf
+pdftk design_b_design.pdf design_b_crop.pdf cat output TFSLOT_sticker_B.pdf
---
+++ doc/sticker/qr.sh
@@ -0,0 +1,1 @@
+qrencode -o qr_web_tfslot01.png "https://thunderfly.cz/pl/TFSLOT01" --background="FFFFFF00" --foreground="2e2a2bff" -s12
